6bbf76319bdd3ed38c7c5b807e64ec183bb4a750
puskaar <puskartiwari92@gmail.com>
2019-07-23 13:43:30 +0545

    manage responsive section

--- divine/main.js
+++ divine/main.js
@@ -1,31 +1,44 @@
-$(document).on('ready', function() {
-		$(".center").slick({
-		dots: false,
-		infinite: true,
-		arrows: false,
-		slidesToShow: 5,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		slidesToScroll: 1,
+$(document).on('ready', function () {
+  $(".center").slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToScroll: 1,
 
- responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        slidesToScroll: 1,
-        slidesToShow: 3
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToScroll: 1,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToScroll: 1,
+          slidesToShow: 3
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        slidesToScroll: 1,
-        slidesToShow: 3
-      }
+    ]
+  });
+  $('#nav-toggler-button').on('click', function () {
+    if ($('#nav-toggler-button').hasClass('nav-add-bar')) {
+      $('#nav-toggler-button').removeClass('nav-add-bar');
+    } else {
+      $('#nav-toggler-button').addClass('nav-add-bar');
     }
-  ]
-
-		});
-		});
+  })
+  $('.dropdown-toggle').on('click', function () {
+    if ($(this).hasClass('dropdown-toggle-a')) {
+      $(this).removeClass('dropdown-toggle-a');
+    } else {
+      $(this).addClass('dropdown-toggle-a');
+    }
+  })
+});
